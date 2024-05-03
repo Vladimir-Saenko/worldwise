300f053595304ebe85d1f253323ac3c495ac3986
Vladimir <siborg.451@gmail.com>
2024-05-03 08:43:41 +0500

    применение useCallback для предотвращения бесконечного цикла

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useEffect, useContext, useReducer } from "react";
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -71,18 +77,21 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === Number(currentCity.id)) return; //отключение повторной загрузки
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === Number(currentCity.id)) return; //отключение повторной загрузки
 
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/info", payload: data });
-    } catch {
-      dispatch({ type: "rejected", payload: "Ошибка загрузки данных..." });
-    }
-  }
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/info", payload: data });
+      } catch {
+        dispatch({ type: "rejected", payload: "Ошибка загрузки данных..." });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
